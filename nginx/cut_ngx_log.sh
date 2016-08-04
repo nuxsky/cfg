561213f3c169d2120b87718da620a7f6531348c5
nuxsky <nuxsky@sina.com>
2016-08-04 11:16:31 +0800

    cut_ngx_log v1.0.2: 修复错误

--- nginx/cut_ngx_log.sh
+++ nginx/cut_ngx_log.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# v1.0.1
+# v1.0.2
 
 DATE=`date +%Y%m%d`
 NGXBIN=/usr/local/nginx
 NGXLOG=/data/logs/nginx
 
 [ -d $NGXLOG ] && cd $NGXLOG || exit 1
-for LOGNAME in `ls $NGXLOG | grep -v "[^0-9].log"`
+for LOGNAME in `ls $NGXLOG | grep -v "[0-9].log"`
 do
   mv $LOGNAME ${LOGNAME%%.*}_$DATE.log
 done
